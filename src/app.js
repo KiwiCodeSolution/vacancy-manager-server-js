85cfeaf456a0ee327a4491e3d7eeedccb4db11b2
AlexeyTolmachov <taar12sh@gmail.com>
2022-11-21 21:22:20 +0200

    fixed bad query

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+
 const { createUser, getUsers } = require('./controller/userController');
 
 app.use(express.json());
--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,17 +1,19 @@
 const userModel = require('../dbMongo/mongoShema/userShema');
 
-module.exports.getUsers = async (req, res, next) => {
+module.exports.createUser = async (req, res, next) => {
   try {
+    console.log(req.body);
     const { body } = req;
     const user = await userModel.create(body);
-
     res.status(201).send({ data: user });
   } catch (error) {
+    // eslint-disable-next-line no-console
+
     next(error);
   }
 };
 
-module.exports.createUser = async (req, res, next) => {
+module.exports.getUsers = async (req, res, next) => {
   try {
     const getUsers = await userModel.find();
 
