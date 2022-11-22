13a4dd13ce523d9a65e209b051130b969eb512ea
Aleksandr-Pod <stdfire@gmail.com>
2022-11-22 23:50:52 +0200

    cors commented

--- src/app.js
+++ src/app.js
@@ -4,18 +4,18 @@ const userRouter = require("./routes/users");
 
 const app = express();
 
-app.use(cors);
+// app.use(cors);
 
 app.use(express.json());
 
-// app.use("/", userRouter);
-app.get('/', (req, res) => res.send({message: "OK"}))
+app.use("/", userRouter);
+// app.get('/', (req, res) => res.send({message: "OK"}))
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-app.use(async (err, req, res) => {
+app.use(async (err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
