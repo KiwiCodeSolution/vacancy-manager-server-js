87100a9779ebcb87536f1374beea75484b0f379e
Aleksandr-Pod <stdfire@gmail.com>
2022-11-22 23:04:25 +0200

    VacancySchema

--- src/dbMongo/models/vacancyModel.js
+++ src/dbMongo/models/vacancyModel.js
@@ -1,7 +1,15 @@
 const mongoose = require("mongoose");
 
 const VacancySchema = new mongoose.Schema({
-  name: { type: String },
+  date: {type: Date},
+  companyName: { type: String },
+  CompanyURL: {type: String},
+  sourse: {type: String},
+  sourseURL: {type: String},
+  position: {type: String},
+  salary: {type: Number},
+  status: {type: String},
+  rank: {type: Number}
 });
 
 const VacancyModel = ("Vacancy", VacancySchema);
