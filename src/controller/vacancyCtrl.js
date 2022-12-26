d567862c2df845e6518cd3a71cd62df7781e01b8
Aleksandr-Pod <stdfire@gmail.com>
2022-12-26 17:52:12 +0200

    corrections 3

--- src/controller/vacancyCtrl.js
+++ src/controller/vacancyCtrl.js
@@ -1,5 +1,5 @@
 const { VacancyModel } = require("../dbMongo/models/vacancyModel");
-const { BadRequest, NotFound } = require("http-errors");
+const { NotFound } = require("http-errors");
 
 const get = async (req, res) => {
     const vacancies = await VacancyModel.find({userId: req.body.userId, ...req.query});
@@ -10,7 +10,7 @@ const get = async (req, res) => {
 };
 
 const create = async (req, res) => {
-    const { userId, companyName, companyURL, source, sourceURL, position, salary, status = "new", rank = 1 } = req.body;
+    const { userId, companyName, companyURL, source, sourceURL, position = "", salary, notes = "", status = "new", rank = 1 } = req.body;
     const vacancy = await VacancyModel.create({ userId, companyName, companyURL, source, sourceURL, position, salary, notes, status, rank });
     res.json({
         message: "Vacancy created",
@@ -36,7 +36,7 @@ const update = async (req, res) => {
 
     const newVacancy = await VacancyModel.findOneAndUpdate({ _id: id, userId }, { companyName, companyURL, source, sourceURL, position, salary, status, rank }, { new: true });
     if (!newVacancy) throw NotFound (`A vacancy with id:${id} not found`);
-    
+
     res.json({
         message: "Vacancy updated",
         newVacancy
