52aa5cf89c77ac3ced1570c20a4ab702b7b36eb8
Aleksandr-Pod <stdfire@gmail.com>
2023-02-28 14:36:54 +0200

    username to profile

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -11,13 +11,12 @@ module.exports.registration = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) throw new BadRequest("ошибка при валидации");
 
-  const { username = "unonymous", password, email } = req.body;
+  const { password, email } = req.body;
   const candidate = await UserModel.findOne({ email });
   if (candidate) throw new Conflict("пользователь с таким имейлом уже существует");
 
   const hashPassword = bcrypt.hashSync(password, 7);
   const user = new UserModel({
-    username,
     password: hashPassword,
     email,
     token: null,
@@ -44,7 +43,7 @@ module.exports.login = async (req, res) => {
 module.exports.logout = async (req, res) => {
   req.user.token = null;
   await req.user.save();
-  res.json({ message: `User ${req.user.username} logged out` });
+  res.json({ message: `User ${req.user.profile.username} logged out` });
 };
 
 module.exports.getUser = async (_req, res) => {
@@ -54,6 +53,6 @@ module.exports.getUser = async (_req, res) => {
 
 module.exports.getCurrent = async (req, res) => {
   console.log("getCurrent");
-  const { username, email } = req.user;
-  res.json({ username, email });
+  const { email, profile } = req.user;
+  res.json({ email, profile });
 }
