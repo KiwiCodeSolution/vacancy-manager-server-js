7c9db9c34459e783637f09a08788d85bd96b6e53
Aleksandr-Pod <stdfire@gmail.com>
2023-03-29 21:25:30 +0300

    update archived

--- src/controller/vacancyCtrl.js
+++ src/controller/vacancyCtrl.js
@@ -2,43 +2,117 @@ const { VacancyModel } = require("../dbMongo/models/vacancyModel");
 const { BadRequest, NotFound } = require("http-errors");
 
 const get = async (req, res) => {
-    const data = await VacancyModel.find({userId: req.user._id, ...req.query});
-    res.json({ data });
+  const data = await VacancyModel.find({ userId: req.user._id, ...req.query });
+  res.json({ data });
 };
 
 const create = async (req, res) => {
-    const { companyName, companyURL, source, sourceURL, position = "", salary, currency = "USD", notes, status = "new", userRank = 1, cardColor = "grey" } = req.body;
-    const data = await VacancyModel.create({ userId: req.user._id, companyName, companyURL, source, sourceURL, position, salary, currency, notes, status, userRank, cardColor, archived: false });
+  const {
+    companyName,
+    companyURL,
+    source,
+    sourceURL,
+    position = "",
+    salary,
+    currency = "USD",
+    notes,
+    status = "new",
+    userRank = 1,
+    cardColor = "grey",
+  } = req.body;
+  const data = await VacancyModel.create({
+    userId: req.user._id,
+    companyName,
+    companyURL,
+    source,
+    sourceURL,
+    position,
+    salary,
+    currency,
+    notes,
+    status,
+    userRank,
+    cardColor,
+    archived: false,
+  });
 
-    res.json({
-        message: "A new vacancy created successfully",
-        data
-    });
+  res.json({
+    message: "A new vacancy created successfully",
+    data,
+  });
 };
 
 const update = async (req, res) => {
-    const { _id, companyName, companyURL, source, sourceURL, position, salary, status, actions, notes, userRank, cardColor, archived } = req.body;
-    if (!companyName && !companyURL && !source && !sourceURL && !position && !salary && !status && !actions && !notes && !userRank && !cardColor && !archived) throw BadRequest("no fields to update");
-    
-    const data = await VacancyModel.findOneAndUpdate({ _id, userId: req.user._id }, { companyName, companyURL, source, sourceURL, position, salary, status, actions, notes, userRank, cardColor, archived }, { new: true });
-    if (!data) throw NotFound (`A vacancy with id:${_id} not found`);
+  const {
+    _id,
+    companyName,
+    companyURL,
+    source,
+    sourceURL,
+    position,
+    salary,
+    status,
+    actions,
+    notes,
+    userRank,
+    cardColor,
+    archived,
+  } = req.body;
+  if (
+    !companyName &&
+    !companyURL &&
+    !source &&
+    !sourceURL &&
+    !position &&
+    !salary &&
+    !status &&
+    !actions &&
+    !notes &&
+    !userRank &&
+    !cardColor &&
+    !(archived === true || archived === false)
+  )
+    throw BadRequest("no fields to update");
 
-    res.json({
-        message: "Vacancy updated successfully",
-        data
-    });
+  const data = await VacancyModel.findOneAndUpdate(
+    { _id, userId: req.user._id },
+    {
+      companyName,
+      companyURL,
+      source,
+      sourceURL,
+      position,
+      salary,
+      status,
+      actions,
+      notes,
+      userRank,
+      cardColor,
+      archived,
+    },
+    { new: true }
+  );
+  if (!data) throw NotFound(`A vacancy with id:${_id} not found`);
+
+  res.json({
+    message: "Vacancy updated successfully",
+    data,
+  });
 };
 
 const remove = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const data = await VacancyModel.findOneAndDelete({ _id: id, userId: req.user._id });
-    if (!data) throw NotFound (`A vacancy with id:${id} not found`);
+  const data = await VacancyModel.findOneAndDelete({
+    _id: id,
+    userId: req.user._id,
+  });
+  if (!data) throw NotFound(`A vacancy with id:${id} not found`);
 
-    res.json({
-        message: "Vacancy removed successfully",
-        data
-    });
+  res.json({
+    message: "Vacancy removed successfully",
+    data,
+  });
 };
 
 module.exports = { get, create, remove, update };
