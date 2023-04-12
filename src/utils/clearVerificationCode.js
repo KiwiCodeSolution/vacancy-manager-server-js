82c8d8948b4b6ed1175ab80247b205078373e04b
Aleksandr-Pod <stdfire@gmail.com>
2023-04-12 15:13:34 +0300

    fix

--- src/utils/clearVerificationCode.js
+++
@@ -1,7 +0,0 @@
-const UserModel = require("../dbMongo/models/UserModel");
-
-const clearVerificationCode = async (id) => {
-  console.log("clear verify code from user id:", id);
-  UserModel.findByIdAndUpdate( id, { verificationCode: "" });
-};
-module.exports = clearVerificationCode;
