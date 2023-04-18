262f1c974334997dcbe8d0cf0480d7bc4c44cf05
Aleksandr-Pod <stdfire@gmail.com>
2023-04-18 08:49:23 +0300

    fix request profileData

--- src/controller/profileCtrl.js
+++ src/controller/profileCtrl.js
@@ -7,7 +7,7 @@ module.exports.update = async (req, res) => {
   if (typeof(profileData) !== "object") throw new BadRequest("profileData shoul be an object");
   if (Array.isArray(profileData)) throw new BadRequest("profileData shoul be an object");
   
-  req.user.profile = { ...req.user.profile, profileData};
+  req.user.profile = { ...req.user.profile, ...profileData};
   await req.user.save();
   res.json({message: "profile updated", profile: req.user.profile});
 };
