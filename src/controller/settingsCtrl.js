f2b7e8cf42df5d4a3f5e593cbfbd33ecf3540e33
Aleksandr-Pod <stdfire@gmail.com>
2023-04-19 15:11:37 +0300

    settings controller

---
+++ src/controller/settingsCtrl.js
@@ -0,0 +1,13 @@
+const { BadRequest } = require("http-errors");
+
+module.exports.update = async (req, res) => {
+  const settingsData = req.body;
+
+  if (!settingsData) throw new BadRequest("no settings data to update");
+  if (typeof(profileData) !== "object") throw new BadRequest("settings data shoul be an object");
+  if (Array.isArray(settingsData)) throw new BadRequest("settings sata shoul be an object");
+  
+  req.user.settings = { ...req.user.settings, ...settingsData};
+  await req.user.save();
+  res.json({message: "settings updated", settings: req.user.settings});
+};
