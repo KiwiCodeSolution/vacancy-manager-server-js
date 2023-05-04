5bfbfdd7c402fd3eb6767a1a91ac580b869b7063
Aleksandr Podmazko <stdfire@gmail.com>
2023-05-04 17:35:00 +0300

    uploadAvatar

--- src/controller/profileCtrl.js
+++ src/controller/profileCtrl.js
@@ -1,4 +1,6 @@
 const { BadRequest } = require("http-errors");
+const cloudinary = require("cloudinary").v2;
+require("dotenv").config();
 
 module.exports.update = async (req, res) => {
   const profileData = req.body;
@@ -10,4 +12,19 @@ module.exports.update = async (req, res) => {
   req.user.profile = { ...req.user.profile, ...profileData};
   await req.user.save();
   res.json({message: "profile updated", profile: req.user.profile});
+};
+
+module.exports.uploadAvatar = async (req, res) => {
+  const file = req.body.data;
+  const { CLOUD_NAME, CLOUD_APIKEY, CLOUD_APISECRET} = process.env;
+
+  cloudinary.config({
+    cloud_name:CLOUD_NAME,
+    api_key:CLOUD_APIKEY,
+    api_secret:CLOUD_APISECRET
+  });
+
+  const uploadedResponse = await cloudinary.uploader.upload(file, {folder: "Avatars"});
+  req.body = {avatar: uploadedResponse.url};
+  this.update(req, res);
 };
