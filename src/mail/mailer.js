1aa3b5f9471966e3dda66731b3cdb6472cfe01b2
Aleksandr-Pod <stdfire@gmail.com>
2023-06-10 13:45:25 +0300

    mailer use env

--- src/mail/mailer.js
+++ src/mail/mailer.js
@@ -1,25 +1,27 @@
 const nodemailer = require("nodemailer");
+require("dotenv").config();
+const { MAIL_HOST, MAIL_FROM, MAIL_PASS } = process.env;
 
 const transporter = nodemailer.createTransport({
-    host: "mail.kiwicode.tech", //не трогать
+    host: MAIL_HOST, //не трогать
     port: 465, //не трогать
     secure: true,
     auth: {
-        user: "leha@kiwicode.tech", // почта юзера или сервиса на кивикоде
-        pass: "11111111", // замените на пароль от учетной записи
+        user: MAIL_FROM, // почта юзера или сервиса на кивикоде
+        pass: MAIL_PASS, // замените на пароль от учетной записи
     },
 });
 
 function sendEmail({ email, html, letterSubject }) {
     const mailOptions = {
-        from: "leha@kiwicode.tech", // от кого
+        from: MAIL_FROM, // от кого
         to: email, //кому
         subject: letterSubject, //Тему письма писать обязательно  приоретет выставляеться выше ! больше шансов не попасть в спам
         html, // тело сообщения
     };
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-            console.log(error);
+            console.log("nodemailer error:", error);
         } else {
             console.log("Email sent: " + info.response);
         }
